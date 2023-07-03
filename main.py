59ada36795c8ab228121721454c492f0c8f53191
ADITYA KADAM <aditya.kadam2205@gmail.com>
2023-07-03 21:52:53 +0530

    added code for line and bet

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import random
+
+MAX_LINES = 3
+MAX_BET  = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
 # To add the deposit to slot machine
 def deposit():
     while True:
@@ -13,4 +28,47 @@ def deposit():
     
     return amount
 
-deposit()
+def get_no_of_lines():
+    while True:
+        lines = input("Enter the no of line you want to bet on (1-" + str(MAX_LINES) + ") ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter the valid no of line")
+        else:
+            print("Please enter a number. ")
+    
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line ? ₹")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be in between ₹{MIN_BET} - ₹{MAX_BET}.")
+        else:
+            print("Please enter a number. ")
+    
+    return amount
+
+
+def main():
+    balance = deposit()
+    lines = get_no_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is:{balance}")
+        else:
+            break
+
+    print(f"You are betting ₹{bet} on {lines} lines. Total bet is equal to: ₹{total_bet}")
+main()
